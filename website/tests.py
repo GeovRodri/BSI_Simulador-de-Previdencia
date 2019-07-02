017aac8032b46df133a17f3e0e2d6a2b55dd60cd
GeovRodri <geovanyscv@gmail.com>
2019-07-02 15:28:37 -0300

    criando testes com o unittest

--- website/tests.py
+++ website/tests.py
@@ -1,8 +1,3 @@
 from django.test import TestCase
-from website.models import Simulacoes
 
-
-class SimulacoesTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.assertEquals("Oi", "Oi")
+# Create your tests here.
